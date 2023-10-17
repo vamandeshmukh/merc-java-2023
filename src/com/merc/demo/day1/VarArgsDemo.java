5c222675f575f4af123a666964f083e3251c8a52
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-10-17 13:00:52 +0530

    java day-1

--- src/com/merc/demo/day1/VarArgsDemo.java
+++ src/com/merc/demo/day1/VarArgsDemo.java
@@ -1,16 +1,23 @@
 package com.merc.demo.day1;
 
 public class VarArgsDemo {
-	
-	static void addNums() {
-		
-		
-		
+
+	static void addNums(int... args) {
+
+		int sum = 0;
+		for (int i : args) {
+			sum += i;
+		}
+		System.out.println(sum);
+
 	}
-	
+
 	public static void main(String[] args) {
-		
-		
+
+		VarArgsDemo.addNums();
+		VarArgsDemo.addNums(10, 20);
+		VarArgsDemo.addNums(10, 20, 30, 40, 50);
+
 	}
 
 }
