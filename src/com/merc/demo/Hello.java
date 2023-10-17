57228e9c2963b4eb71827b6030037605b73f2c9b
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-10-17 10:57:35 +0530

    java day-1

--- src/com/merc/demo/Hello.java
+++ src/com/merc/demo/Hello.java
@@ -1,9 +1,50 @@
 package com.merc.demo;
 
+/*
+Java fundamentals 
+Building blocks of Java
+
+keywords 
+datatypes 
+idnetifiers 
+literals 
+operators 
+separators 
+white space 
+comments 
+
+control structure 
+
+datatypes in Java - 
+
+primitive datatypes - store one single value 
+- integer -          byte short int long char 
+- floating types -   float double 
+- character values - char 
+- boolean - boolean 
+
+reference datatypes - store group of values 
+
+ 
+
+*/
+
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+
+		int num = 10;
+		double num2 = 20;
+
+		System.out.println(num);
+		System.out.println(num2);
+
 	}
 
 }
+
+/*
+ * console.log('Hello world!'); console.log("Hello world!"); console.log(`Hello
+ * world!'); console.log('Hello world!') console.log("Hello world!")
+ * console.log(`Hello world!')
+ */
