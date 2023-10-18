68128b3bdf3040ca3e098288717268b616f04d33
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-10-18 12:03:35 +0530

    java day-2

--- src/com/merc/demo/day2/misc/RandomDemo.java
+++ src/com/merc/demo/day2/misc/RandomDemo.java
@@ -3,12 +3,23 @@ package com.merc.demo.day2.misc;
 import java.util.Random;
 
 public class RandomDemo {
-	
+
+	public static void randomNums() {
+		Random random = new Random();
+		for (int i = 1; i <= 10; i++) {
+//			int num = random.nextInt();
+			int num = random.nextInt(100); // specify range 0 - 100
+			System.out.println(num);
+		}
+	}
+
 	public static void main(String[] args) {
+
+//		Random random = new Random();
+//		int num = random.nextInt();
+//		System.out.println(num);
 		
-		Random random = new Random();		
-		int num = random.nextInt();
-		System.out.println(num);
+		RandomDemo.randomNums();
 
 	}
 
