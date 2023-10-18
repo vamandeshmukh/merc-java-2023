638ebec7ab098b53eaa36c3bdc795db9e9484810
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-10-18 14:39:45 +0530

    java day-2

--- src/com/merc/demo/day2/col/BlogDemo.java
+++ src/com/merc/demo/day2/col/BlogDemo.java
@@ -11,10 +11,10 @@ public class BlogDemo {
 		System.out.println(blogList);
 		System.out.println(blogList.size());
 
-		blogList.add(new Blog(10, "Some Title", "Some body"));
-		blogList.add(new Blog(11, "Some Title 2", "Some body 2"));
-		blogList.add(new Blog(12, "Some Title 3", "Some body 3"));
-		blogList.add(new Blog(13, "Some Title 4", "Some body 4"));
+		blogList.add(new Blog(11, "Some Title", "Some body"));
+		blogList.add(new Blog(12, "Some Title 2", "Some body 2"));
+		blogList.add(new Blog(13, "Some Title 3", "Some body 3"));
+		blogList.add(new Blog(14, "Some Title 4", "Some body 4"));
 		System.out.println("Print all blog objects");
 
 		for (Blog blog : blogList) {
@@ -22,7 +22,7 @@ public class BlogDemo {
 		}
 
 		System.out.println("Find blog based on blogId:");
-		int blogIdToSearch = 11;
+		int blogIdToSearch = 13;
 		for (Blog blog : blogList) {
 			if (blog.blogId == blogIdToSearch)
 				System.out.println(blog.toString());
